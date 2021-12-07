60adf089f94e9374621463991c37d3d7b1bcbff5
AugustoMagalhaes <augustommg@gmail.com>
2021-12-07 02:26:45 -0300

    Adicionando resolucao do desafio 13.

--- src/challenges2.js
+++ src/challenges2.js
@@ -25,8 +25,15 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(amountOfDrinks) {
+  let splitedAmountOfDrinks = amountOfDrinks.split(' ');
+  let numberOfHydrates = 0;
+  for (let i = 0; i < splitedAmountOfDrinks.length; i += 1) {
+    if (isNaN(splitedAmountOfDrinks[i]) === false) {
+      numberOfHydrates += parseInt(splitedAmountOfDrinks[i], 10);
+    }
+  }
+  return (numberOfHydrates === 1) ? `${numberOfHydrates} copo de água` : `${numberOfHydrates} copos de água`;
 }
 
 module.exports = {
